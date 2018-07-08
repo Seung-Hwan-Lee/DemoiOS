458d7d10bfd88a31e4384df693a42c7e3f9d20d1
shwan.lee <shwan.lee@linecorp.com>
2018-07-08 11:45:14 +0900

    Initial Commit

---
+++ DemoiOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DemoiOS
+//
+//  Created by SeungHwan Lee on 2018. 7. 8..
+//  Copyright © 2018년 Thomas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
